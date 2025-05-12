9679defe9ae19f3ab972d3bf6c8e6740f939b299
Akmal Khan <akmalnawabi007@gmail.com>
2025-05-12 22:30:03 +0430

    new

--- src/app/snippet/new/page.tsx
+++ src/app/snippet/new/page.tsx
@@ -22,20 +22,20 @@ export default function createSnippetPage(){
     return (
         <form action={snippetCreate}>
             <h3 className="font-bold m-3">Create a snippet</h3>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 justify-center mx-12">
                 <div className="flex gap-4">
                     <label className="w-12" htmlFor="title">Title</label>
                     <input className="border rounded p-2 w-full" name="title" id="title" />
                 </div>
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4 justify-center mx-12">
                 <div className="flex gap-4">
                     <label className="w-12" htmlFor="code">Code</label>
                     <textarea className="border rounded p-2 w-full" name="code" id="code" />
                 </div>
             </div>
 
-            <button type="submit" className="rounded p-2 bg-blue-200 w-full">Create</button>
+            <button type="submit" className="rounded p-2 bg-blue-200 w-1/2 mx-26">Create</button>
         </form>
     )
 }
