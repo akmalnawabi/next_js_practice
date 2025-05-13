43cebddd058a6cc374f7b2953bb83d0028da3e0d
Akmal Khan <akmalnawabi007@gmail.com>
2025-05-13 22:24:03 +0430

    new

--- src/app/scale/page.tsx
+++ src/app/scale/page.tsx
@@ -1,19 +1,29 @@
 import scaleImg from 'public/boy.jpg';
 import Hero from '../../components/hero';
-import Snippet from '../snippet/new/page'
 import { db } from '@/src/db';
+import Link from 'next/link';
 
 export default async function scalePage() {
     const snippets = await db.snippet.findMany();
     const renderedSnippets = snippets.map((snippet) => {
-        return <div key={snippet.id}>{snippet.title}</div>
+        return <Link key={snippet.id}
+            href={`/snippet/${snippet.id}`}
+            className='flex justify-between items-center p-2 border rounded mx-8 my-2'>
+            <div>{snippet.title}</div>
+            <div>View</div>
+        </Link>
     })
+
     return (
         <div>
             <Hero imgData={scaleImg} imgAlt='ronaldo' title='beckham' />
-            <Snippet />
-            <div className='text-center my-1 font-bold'>
-                {renderedSnippets}
+            {/* <Snippet /> */}
+            <div className='flex flex-col gap-2 mt-8'>
+                <div className='flex justify-between mx-8'>
+                    <h2 className='text-xl font-bold'>Snippet</h2>
+                    <Link href='/snippet/new' className='border rounded p-2'>New</Link>
+                </div>
+                <div>{renderedSnippets}</div>
             </div>
         </div>
     )
