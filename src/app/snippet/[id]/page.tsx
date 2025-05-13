a352ae9752211a784e496e2650e45b292a1f8570
Akmal Khan <akmalnawabi007@gmail.com>
2025-05-13 22:24:36 +0430

    new

--- src/app/snippet/[id]/page.tsx
+++ src/app/snippet/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { db } from "@/src/db";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 interface SnippetShowPageProps {
@@ -7,15 +8,28 @@ interface SnippetShowPageProps {
     }
 }
 
-export default async function showSnippetPage(props: SnippetShowPageProps){
+export default async function showSnippetPage(props: SnippetShowPageProps) {
 
     const snippet = await db.snippet.findFirst({
-        where: {id: parseInt(props.params.id)}
+        where: { id: parseInt(props.params.id) }
     })
 
-    if(!snippet){
+    if (!snippet) {
         return notFound()
     }
 
-    return <div>{snippet.title}</div>
+    return <div>
+        <div className="flex m-4 justify-between items-center">
+            <h1 className="text-xl font-bold">
+                {snippet.title}
+            </h1>
+            <div>
+                <Link key={snippet.id} href={`/snippet/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
+                <button className="p-2 border rounded">Delete</button>
+            </div>
+        </div>
+        <pre className="ml-5">
+            <h5>{snippet.code}</h5>
+        </pre>
+    </div>
 }
